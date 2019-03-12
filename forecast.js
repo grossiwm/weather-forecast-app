c41c5fa5437e80e7f061ef6d5b74922a57e10261
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2019-03-12 19:19:31 -0300

    Changing string sum to string interpolation.

--- forecast.js
+++ forecast.js
@@ -3,7 +3,7 @@ const personal = require('./environments/developer')
 
 const forecast = ({latitude, longitude, place_name}, callback) => {
     const access_token = personal.dark_sky_key
-    const url = 'https://api.darksky.net/forecast/'+ access_token + '/' + latitude + ', ' + longitude
+    const url = `https://api.darksky.net/forecast/${access_token}/${latitude},${longitude}?lang=pt&units=si`
     request({url, json: true}, (error, response) => {
         console.log(place_name)
         callback(response.body)
