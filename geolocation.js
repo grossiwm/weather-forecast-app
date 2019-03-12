a3fc80c304e17d63cddd7a80521484f04d4af6e7
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2019-03-12 19:05:51 -0300

    Using environment constants to store api keys.

--- geolocation.js
+++ geolocation.js
@@ -1,12 +1,15 @@
 const request = require('request')
+const personal = require('./environments/developer')
 
 const geolocation = (address, callback) => {
-    const access_token = process.env.map_box_key
+    const access_token = personal.map_box_key
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=' + access_token
 
     request({url, json: true}, (error, response) => {
         if (error) {
             callback('Unable to connect to geolocation services!', undefined)
+        }else if(response.body.features.length === 0){
+            callback('Unable to find address.', undefined)
         }else{
             callback(undefined, {longitude: response.body.features[0].center[0], latitude: response.body.features[0].center[1], place_name: response.body.features[0].place_name})
         }
